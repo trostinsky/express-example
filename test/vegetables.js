beede3de8731144879790814102d2801d5bf8273
Vlad <gribennikov.vlad@gmail.com>
2018-04-11 21:07:37 +0300

    added test

---
+++ test/vegetables.js
@@ -0,0 +1,28 @@
+const chai = require("chai");
+const chaiHttp = require('chai-http');
+const expect = chai.expect;
+
+chai.use(chaiHttp);
+
+const name = 'Abushit';
+
+describe("----> Test for testing", () => {
+    it("/vegetables/ ---> GET", () => {
+        chai.request("http://localhost:9999")
+        .get("/vegetables/").send()
+        .then((response) => {
+            expect(response.body.status).to.equal(200)
+        });
+    });
+    it("/vegetables/ ---> POST", (done) => {
+        chai.request("http://localhost:9999")
+            .post("/vegetables/").send()
+            .then((response) => {
+                expect(response.body.status).to.equal(400)
+                done();
+            })
+            .catch((e) => {
+                done(e);
+            })
+    });
+});
